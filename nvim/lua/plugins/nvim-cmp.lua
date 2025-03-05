07b9d7ce1b78b5b9a61d6d6183845622c7c18c33
Nirerp <102354696+Nirerp@users.noreply.github.com>
2025-03-05 15:32:56 +0200

    Update nvim-cmp.lua

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -35,8 +35,9 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				-- Change these keybindings to avoid conflict with window navigation
+				["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion (was C-k)
+				["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion (was C-j)
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
