5cf1cb7953ee54ea82c1b14318e3d7fd4ef9dee0
Nirerp <102354696+Nirerp@users.noreply.github.com>
2025-03-05 14:50:10 +0200

    Update codeium.lua

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -6,21 +6,37 @@ return {
     },
     config = function()
         require("codeium").setup({
-            -- Use your existing API key
-            api_key = "ENTER YOUR CODEIUM API KEY HERE",
+            -- Disable nvim-cmp integration to use virtual text instead
+            enable_cmp_source = false,
             
             -- Enable virtual text for inline suggestions
             virtual_text = {
                 enabled = true,
-                -- These are the defaults
-                manual = false,
                 idle_delay = 75,
                 key_bindings = {
                     accept = "<Tab>",
                     next = "<M-]>",
                     prev = "<M-[>",
+                    dismiss = "<C-]>",
                 }
             }
         })
+        
+        -- Create commands to enable, disable, and toggle Codeium
+        vim.api.nvim_create_user_command("CodeiumEnable", function()
+            require("codeium.config").options.virtual_text.enabled = true
+            vim.notify("Codeium enabled", vim.log.levels.INFO)
+        end, {})
+        
+        vim.api.nvim_create_user_command("CodeiumDisable", function()
+            require("codeium.config").options.virtual_text.enabled = false
+            vim.notify("Codeium disabled", vim.log.levels.INFO)
+        end, {})
+        
+        vim.api.nvim_create_user_command("CodeiumToggle", function()
+            local virtual_text = require("codeium.config").options.virtual_text
+            virtual_text.enabled = not virtual_text.enabled
+            vim.notify("Codeium " .. (virtual_text.enabled and "enabled" or "disabled"), vim.log.levels.INFO)
+        end, {})
     end,
 }
