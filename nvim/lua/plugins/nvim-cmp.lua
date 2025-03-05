82d8aed0c6f5af7094e826b6227d8120b91755b9
Nirerp <102354696+Nirerp@users.noreply.github.com>
2025-03-05 14:50:27 +0200

    Update nvim-cmp.lua

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -36,7 +36,7 @@ return {
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
@@ -45,7 +45,6 @@ return {
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				{ name = "codeium" }, -- ADD THIS LINE INCASE YOU WISH TO USE CODEIUM
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
@@ -57,7 +56,6 @@ return {
 				format = lspkind.cmp_format({
 					maxwidth = 50,
 					ellipsis_char = "...",
-					symbol_map = { Codeium = "", }, -- Add this line
 				}),
 			},
 		})
